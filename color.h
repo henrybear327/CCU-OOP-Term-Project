1f4ca0cf972f448004b836c5d539c1d9dc67a0ac
Henry Tseng <henrybear327@gmail.com>
2017-05-27 00:00:59 +0800

    Fix typo in color.h

--- color.h
+++ color.h
@@ -7,7 +7,7 @@ For terminal to display color output.
 
 #define RED "\x1B[31m"
 #define GREEN "\x1B[32m"
-#define YELLOE "\x1B[33m"
+#define YELLOW "\x1B[33m"
 #define BLUE "\x1B[34m"
 #define CYAN "\x1B[36m"
 #define WHITE "\x1B[37m"
